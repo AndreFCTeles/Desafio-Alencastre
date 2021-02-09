6b0257d4c487aff5728b22fe1ad06377b8ea1051
AndreTeles <andrefcteles@gmail.com>
2021-02-09 19:36:03 +0000

    commit

--- public/CustomJS.js
+++ public/CustomJS.js
@@ -1,12 +1,32 @@
 $(document).ready(function () {
 
 
-    var getWeather = function(){
+    $("#goSearch").click(function(){
 
         var key = '303bda03f4411976f702bf902ea85f54'; // API key
-        var cityName = $('CitySearch').val(); // search input id
+        var cityName = $('#CitySearch').val(); // search input id
 
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key + '&units=metric')
+        .then(function(resp){ return resp.json() })
+        .then(function(){
+            $('#cityname').html(name);
+            $('#countrycode').html(country);
+            $('#coords').html('[' + lat + ', ' + lon + ']');
+            
+            $('#date').html(dt);
 
+            $('#temp').html(temp);
+            //if para o icone
+
+            $('#maxmin').html(temp_max + '&deg; / ' + temp_min + '&deg;');
+            $('#wind').html(speed + 'm/s');
+            $('#humidity').html(humidity);
+            $('#pressure').html(pressure);
+        })
+        .catch(function(){
+            //catch errors
+        });
+    });
 
 
 
