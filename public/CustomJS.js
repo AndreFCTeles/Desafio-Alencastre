caca579ea703fb1a9d7e5bb54b29e7a36f9d4f16
AndreTeles <andrefcteles@gmail.com>
2021-02-09 23:21:00 +0000

    commit

--- public/CustomJS.js
+++ public/CustomJS.js
@@ -1,45 +1,69 @@
+$(document).one('ready', function() {
+    $.ajax({ // calling API
+        type: "POST",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=303bda03f4411976f702bf902ea85f54&units=metric",
+        dataType: "json",
+        success: function(result) {
+            console.log(result)
+                
+            // Populating HTML
+            $('#cityname').html(result.name);
+            $('#countrycode').html(result.sys.country);
+            $('#coords').html('[' + result.coord.lat + ', ' + result.coord.lon + ']');
+            $('#date').html(result.dt);    
+            $('#temp').html(result.main.temp);
+                //if para o icone aqui, ou noutra function / outro método?    
+            $('#maxmin').html(result.main.temp_max + '&deg; / ' + result.main.temp_min + '&deg;');
+            $('#wind').html(result.wind.speed + 'm/s');
+            $('#humidity').html(result.main.humidity);
+            $('#pressure').html(result.main.pressure);
+        },
+        error: function(xhr, status, error) {
+            alert("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+        }
+    });
+});
+
 $(document).ready(function () {
 
-
-    $("#goSearch").click(function(){
-
+    $("#goSearch").click(function(e){        
         var key = '303bda03f4411976f702bf902ea85f54'; // API key
-        var cityName = $('#CitySearch').val(); // search input id
+        var citySearchName = $('#CitySearch').val(); // search input id
 
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key + '&units=metric')
-        .then(function(resp){ return resp.json() })
-        .then(function(){
-            $('#cityname').html(name);
-            $('#countrycode').html(country);
-            $('#coords').html('[' + lat + ', ' + lon + ']');
-            
-            $('#date').html(dt);
-
-            $('#temp').html(temp);
-            //if para o icone
-
-            $('#maxmin').html(temp_max + '&deg; / ' + temp_min + '&deg;');
-            $('#wind').html(speed + 'm/s');
-            $('#humidity').html(humidity);
-            $('#pressure').html(pressure);
-        })
-        .catch(function(){
-            //catch errors
+        $.ajax({ // calling API
+            type: "POST",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearchName + "&appid=" + key + "&units=metric",
+            dataType: "json",
+            success: function(result) {
+                console.log(result)
+                
+                // 
+                $('#cityname').html(result.name);
+                $('#countrycode').html(result.sys.country);
+                $('#coords').html('[' + result.coord.lat + ', ' + result.coord.lon + ']');
+                
+                $('#date').html(result.dt);
+    
+                $('#temp').html(result.main.temp);
+                    //if para o icone
+    
+                $('#maxmin').html(result.main.temp_max + '&deg; / ' + result.main.temp_min + '&deg;');
+                $('#wind').html(result.wind.speed + 'm/s');
+                $('#humidity').html(result.main.humidity);
+                $('#pressure').html(result.main.pressure);
+            },
+            error: function(xhr, status, error) {
+                alert("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
         });
     });
+
 
 
 
 });
 
 
-
-
-
-
-
-
-    
 
 
 
