1ceac52867d4b396b6b7ce5dae3188e6abf99735
AndreTeles <andrefcteles@gmail.com>
2021-02-09 18:20:30 +0000

    test

---
+++ CustomJS.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+
+    var getWeather = function(){
+
+        var key = '303bda03f4411976f702bf902ea85f54'; // API key
+        var cityName = $('CitySearch').val(); // search input id
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
